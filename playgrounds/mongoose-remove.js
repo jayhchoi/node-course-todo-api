1383477365dcef067fca558513fdd9879a85c309
JayHChoi <jaychoi1619@gmail.com>
2019-01-25 12:12:53 +0900

    Add DELETE /todos/:id route

---
+++ playgrounds/mongoose-remove.js
@@ -0,0 +1,4 @@
+const { ObjectID } = require('mongodb');
+require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
--- server/server.js
+++ server/server.js
@@ -58,6 +58,26 @@ app.get('/todos/:id', (req, res) => {
     .catch(e => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  // Validate Mongo ObjectId
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // Remove todo by id
+  Todo.findByIdAndDelete(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send(todo);
+    })
+    .catch(e => res.status(400).send());
+});
+
 app.listen(PORT, () => {
   console.log(`Starting on port ${PORT}`);
 });
