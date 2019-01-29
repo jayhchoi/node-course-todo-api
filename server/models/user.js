b0ebf1477942882e439ffa7d49285889d35c6317
JayHChoi <jaychoi1619@gmail.com>
2019-01-29 18:42:00 +0900

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,18 @@ userSchema.methods.generateAuthToken = function() {
   });
 };
 
+userSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  });
+};
+
 userSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
